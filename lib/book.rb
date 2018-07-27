785d3dc3430cb41b03963eca2f4dcc9b36a96caf
gracehillen <grahillen19@ahastars.org>
2018-07-27 19:03:16 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-# book.rb
-# Add you Book class here
+class Book
+  attr_reader :title
+  attr_accessor :author, :page_count, :author, :genre
+  
+  def initialize(title)
+    @title = title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+end
+  book = Book.new("Some Title")
