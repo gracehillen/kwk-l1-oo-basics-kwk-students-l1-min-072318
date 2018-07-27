9f7a37ced65146f0cc50ef08f9628e97811adff0
gracehillen <grahillen19@ahastars.org>
2018-07-27 19:01:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,13 @@ class Shoe
   end
   
   def cobble
+    puts "Your shoe is as good as new!"
+  end 
+  
+  def cobble
     @condition = new
     puts "the shoe has been repaired"
-  end 
+  end
 
 end
 
